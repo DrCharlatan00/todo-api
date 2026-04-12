7514f2717302350a2d28f73694faf5f062ef25db
Dr.Charlatan <sahsasimonenko@gmail.com>
2026-04-12 20:29:03 +0300

    1

---
+++ todo-api/Controllers/TodoItemController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using todo_api.Models;
+
+namespace todo_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemController : ControllerBase
+    {
+        public List<TodoItem> items = new();
+        [HttpGet]
+        public async Task<IActionResult> Get() {
+            return  Ok(items);
+        }
+        public async Task<IActionResult> SearchById(int idProd) {
+            var item = items.Where(p => p.ID == idProd).FirstOrDefault();
+            if (item is null) return NotFound();
+            return Ok(item);
+        }
+        public async Task<IActionResult> RemoveById(int idProd)
+        {
+            var item = items.Where(p => p.ID == idProd).FirstOrDefault();
+            if (item is null) return NotFound();
+            try
+            {
+                items.Remove(item);
+            }
+            catch
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+    }
+}
---
+++ todo-api/Models/TodoItem.cs
@@ -0,0 +1,22 @@
+﻿namespace todo_api.Models
+{
+    public class TodoItem
+    {
+        public TodoItem(int iD, string title, bool isDone)
+        {
+            ID = iD;
+            Title = title;
+            IsDone = isDone;
+        }
+
+        public TodoItem()
+        {
+            
+        }
+
+        public int ID { get; set; } =  0;
+        public string Title { get; set; } = string.Empty;
+
+        public bool IsDone { get; set; } = false;
+    }
+}
