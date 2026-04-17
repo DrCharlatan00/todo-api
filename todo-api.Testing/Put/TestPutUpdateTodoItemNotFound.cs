1826fcd5036a0af87d5da4bf6a60b376916a5386
Dr.Charlatan <sahsasimonenko@gmail.com>
2026-04-17 19:30:55 +0300

    NotFoundTest

---
+++ todo-api.Testing/Put/TestPutUpdateTodoItemNotFound.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using todo_api.Controllers;
+using todo_api.Interfaces;
+using todo_api.Models;
+
+namespace todo_api.Testing;
+
+public class TestPutUpdateTodoItemNotFound
+{
+    [Fact]
+    public async Task TestUpdateNotFound() {
+        var mock = new Mock<ITodoService>();
+
+        int id = 999;
+        var Todo = new TodoItem
+        {
+            ID = id,
+            IsDone = true,
+            Title = "dsf"
+        };
+
+        mock.Setup(s => s.Update(id,It.IsAny<TodoItem>())).ReturnsAsync((TodoItem)null);
+
+        var controller = new TodoItemController(mock.Object);
+
+        var ans = await controller.UpdateTodo(id,Todo);
+
+        Assert.IsType<NotFoundResult>(ans);
+        mock.Verify(s => s.Update(id, Todo), Times.Once);
+    }
+}
